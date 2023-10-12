e907fe4416f4434a28cc23ede15c678dff17c4bf
root <root@runner-sif1rkmc-project-16870191-concurrent-0l6q74>
2023-10-12 11:59:13 +0200

    Release 2.1.6

--- Model/Resolver/RestoreQuote.php
+++ Model/Resolver/RestoreQuote.php
@@ -10,6 +10,7 @@
  */
 namespace Wallee\Payment\Model\Resolver;
 
+use Magento\Authorization\Model\UserContextInterface;
 use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\CustomerGraphQl\Model\Customer\GetCustomer;
 use Magento\Framework\Event\ManagerInterface;
@@ -79,7 +80,16 @@ class RestoreQuote implements ResolverInterface
      */
     private $logger;
 
-
+    /**
+     * @param Session $customerSession
+     * @param CheckoutSession $checkoutSession
+     * @param GetCustomer $getCustomer
+     * @param CartRepositoryInterface $cartRepository
+     * @param OrderRepositoryInterface $orderRepository
+     * @param MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteIdService
+     * @param ManagerInterface $eventManager
+     * @param LoggerInterface $logger
+     */
     public function __construct(Session $customerSession, CheckoutSession $checkoutSession,GetCustomer $getCustomer,
     CartRepositoryInterface $cartRepository, OrderRepositoryInterface $orderRepository,
     MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteIdService, ManagerInterface $eventManager, LoggerInterface $logger) {
@@ -93,28 +103,24 @@ class RestoreQuote implements ResolverInterface
         $this->logger = $logger;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
-        $customerId = null;
-
-        //only perform validations if the user is anonymous.
-        if ($this->checkoutSession->getQuote()->getCustomerId() || !$this->customerSession->getCustomer()->getId()) {
-            /** @var ContextInterface $context */
-            if (false === $context->getExtensionAttributes()->getIsCustomer()) {
-                throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
-            }
-
-            $customer = $this->getCustomer->execute($context);
-            $customerId = $customer->getId();
-
-            if (!empty($this->customerSession) && $customerId !== $this->customerSession->getCustomer()->getId()) {
-                throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
-            }
-        }
-
         try {
             $cartIdMasked = $args['input']['cart_id'];
-            return $this->restoreQuote($cartIdMasked, $customerId);
+
+            if ($context->getUserType() === UserContextInterface::USER_TYPE_GUEST) {
+                return $this->restoreGuestQuote($cartIdMasked);
+            }
+
+            if ($context->getUserType() !== UserContextInterface::USER_TYPE_GUEST) {
+                /** @var ContextInterface $context $customerId */
+                $customerId = $this->getCustomerIdFromContext($context);
+                return $this->restoreCustomerQuote($cartIdMasked, $customerId);
+            }
+            return ['result' => 'There is no cart available'];
         } catch (NoSuchEntityException|\Exception $e) {
             $this->logger->critical($e);
             throw new GraphQlNoSuchEntityException(__($e->getMessage()));
@@ -122,14 +128,14 @@ class RestoreQuote implements ResolverInterface
     }
 
     /**
-     * Restores a client's quote from a cart id
+     * Restores a customer's quote from a cart id
      *
      * @param string $cartIdMasked
      * @param string $customerId
      * @return array
      * @throws LocalizedException
      */
-    private function restoreQuote(string $cartIdMasked, string $customerId)
+    private function restoreCustomerQuote(string $cartIdMasked, string $customerId)
     {
         try {
             // Convert the masked ID to the real quote ID
@@ -144,19 +150,31 @@ class RestoreQuote implements ResolverInterface
             $this->guardQuoteBelongsToCurrentCustomer($order, $customerId);
             $this->guardQuoteIsStillActive($quote);
 
-            //restore a customer's quote
-            $quote->setIsActive(1)->setReservedOrderId(null);
+            return $this->restoreQuote($cartIdMasked, $quote, $order);
+        } catch (NoSuchEntityException|\Exception $e) {
+            return ['result' => 'KO. ' . $e->getMessage()];
+        }
+    }
 
-            $this->cartRepository->save($quote);
-            $this->checkoutSession->replaceQuote($quote)->unsLastRealOrderId();
+    /**
+     * Restores a guest's quote from a cart id
+     *
+     * @param string $cartIdMasked
+     * @return array
+     * @throws LocalizedException
+     */
+    private function restoreGuestQuote(string $cartIdMasked)
+    {
+        try {
+            // Convert the masked ID to the real quote ID
+            $quoteId = $this->maskedQuoteIdToQuoteIdService->execute($cartIdMasked);
 
-            $this->eventManager->dispatch('restore_quote', ['order' => $order, 'quote' => $quote]);
-            $this->logger->debug("RESTORE-QUOTE-MUTATION::restoreQuote - Quote with id $cartIdMasked was restored");
+            // Get the quote using the actual ID
+            /** @var Quote $quote */
+            $quote = $this->cartRepository->get($quoteId);
+            $order = $this->getOrderByQuote($quote);
 
-            return [
-                'result' => 'OK',
-                'cart_id' => $cartIdMasked
-            ];
+            return $this->restoreQuote($cartIdMasked, $quote, $order);
         } catch (NoSuchEntityException|\Exception $e) {
             return ['result' => 'KO. ' . $e->getMessage()];
         }
@@ -174,7 +192,7 @@ class RestoreQuote implements ResolverInterface
         $orderId = $quote->getReservedOrderId();
 
         if (empty($orderId)) {
-            throw new \Exception(__('The quote does not have an associated order'));
+            throw new \Exception(__('The cart does not have an associated order'));
         }
 
         return $this->orderRepository->getOrderById($orderId);
@@ -211,5 +229,62 @@ class RestoreQuote implements ResolverInterface
 
             throw new \Exception(__('The quote is still active.'));
         }
+    }
+
+    /**
+     * Gets the customer id from the user context
+     * @param ContextInterface $context
+     * @param int|null $customerId
+     * @return int|null
+     * @throws GraphQlAuthorizationException
+     * @throws GraphQlNoSuchEntityException
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     * @throws \Magento\Framework\GraphQl\Exception\GraphQlAuthenticationException
+     * @throws \Magento\Framework\GraphQl\Exception\GraphQlInputException
+     */
+    public function getCustomerIdFromContext(ContextInterface $context): mixed
+    {
+        $customerId = null;
+        if ($context->getUserType() === UserContextInterface::USER_TYPE_GUEST) {
+            return $customerId;
+        }
+
+        //only perform validations if the user is anonymous.
+        if ($this->checkoutSession->getQuote()->getCustomerId() || !$this->customerSession->getCustomer()->getId()) {
+            /** @var ContextInterface $context */
+            if (false === $context->getExtensionAttributes()->getIsCustomer()) {
+                throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
+            }
+
+            $customer = $this->getCustomer->execute($context);
+            $customerId = $customer->getId();
+
+            if (!empty($this->customerSession) && $customerId !== $this->customerSession->getCustomer()->getId()) {
+                throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
+            }
+        }
+        return $customerId;
+    }
+
+    /**
+     * Restore a customer or guest's quote
+     * @param Quote $quote
+     * @param OrderInterface|null $order
+     * @return array<mixed>
+     */
+    public function restoreQuote(string $cartIdMasked, Quote $quote, OrderInterface $order = null)
+    {
+        $quote->setIsActive(1)->setReservedOrderId(null);
+
+        $this->cartRepository->save($quote);
+        $this->checkoutSession->replaceQuote($quote)->unsLastRealOrderId();
+        $this->eventManager->dispatch('restore_quote', ['order' => $order, 'quote' => $quote]);
+        $this->logger->debug("RESTORE-QUOTE-MUTATION::restoreQuote - Quote with id $cartIdMasked was restored");
+
+        return [
+            'result' => 'OK',
+            'cart_id' => $cartIdMasked
+        ];
     }
 }
